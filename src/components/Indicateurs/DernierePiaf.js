90fa1dd069313f6bde27f2816f7c01ab783d6133
Pierre Martin <pierre@occitech.fr>
2019-03-05 20:11:21 +0100

    feat(ui): affichage d’un message plutôt que le compteur pour les nouveaux n’ayant pas PIAFé
    sauf si cela fait plus de 4 semaines
    
    closes #19

--- src/components/Indicateurs/DernierePiaf.js
+++ src/components/Indicateurs/DernierePiaf.js
@@ -1,14 +1,24 @@
 import React from "react";
 import { Text } from "rebass";
 
+const AucunePiaf = props => (
+  <Text {...props} color="primary" bg="greyblue">
+    <Text fontSize={4}>Pas encore de PIAF effectuées</Text>
+  </Text>
+);
+
 const DernierePiaf = ({ nbSemaines, date, ...props }) => {
+  const ok = nbSemaines <= 4;
+  const commonProps = {
+    ...props,
+    textAlign: "center"
+  };
+
+  if (ok && date === "-") {
+    return <AucunePiaf {...commonProps} />;
+  }
   return (
-    <Text
-      {...props}
-      color="white"
-      bg={nbSemaines <= 4 ? "green" : "orange"}
-      textAlign="center"
-    >
+    <Text {...commonProps} color="white" bg={ok ? "green" : "orange"}>
       <Text fontSize={8}>{nbSemaines}</Text>
       <Text>semaines depuis la dernière PIAF</Text>
       <Text fontSize={4}>{`(${date})`}</Text>
